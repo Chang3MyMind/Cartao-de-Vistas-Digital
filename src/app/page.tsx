0eb2e7c06e3c19e8cf147ba94d432d7208191775
Matheus Mattos <matheusmattos.r@gmail.com>
2025-10-07 08:59:29 -0300

    feat: calls the bd function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
+import getBdData from "@/api/getBdData";
+
+type BdData = {
+  id: number;
+  title: string;
+  url: string;
+};
+
+export default async function Home() {
+  const data = await getBdData();
+
   return;
 }
