c9975257a29300420530b66b2540a9c8734b2b3e
Matheus Mattos <matheusmattos.r@gmail.com>
2025-11-10 09:53:26 -0300

    feat:Change the delete implementation to the supa db.

--- src/action/deleteLink.ts
+++ src/action/deleteLink.ts
@@ -1,17 +1,20 @@
 "use server";
-import path from "path";
-import { promises as fs } from "fs";
+import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 
 export default async function deleteLink(formData: FormData) {
   const id = formData.get("id");
 
   try {
-    const file = path.join(process.cwd(), "/bd.json");
-    const jsonData = await fs.readFile(file, "utf8");
-    const data = JSON.parse(jsonData);
-    const filteredData = data.filter((item: { id: string }) => item.id !== id);
-    await fs.writeFile(file, JSON.stringify(filteredData, null, 2));
+    const supabase = await createClient();
+
+    const { error } = await supabase.from("user_links").delete().eq("id", id);
+
+    if (error) {
+      console.error("Error deleting link:", error);
+      return;
+    }
+
     revalidatePath("/admin");
     revalidatePath("/");
   } catch (err) {
